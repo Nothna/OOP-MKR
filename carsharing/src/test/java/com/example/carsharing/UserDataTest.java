0aec61bfb08762b936f634b827e4e055df19a51f
Bodomerka <117486637+Bodomerka@users.noreply.github.com>
2023-11-19 12:26:49 +0200

    Дописано функції у DataWriter та написані тести DataWriterTest

--- carsharing/src/test/java/com/example/carsharing/UserDataTest.java
+++ carsharing/src/test/java/com/example/carsharing/UserDataTest.java
@@ -1,117 +1,124 @@
-/*
 package com.example.carsharing;
-import com.example.carsharing.dataWriter.UserData;
-
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.io.FileWriter;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.json.simple.parser.JSONParser;
+import com.example.carsharing.dataWriter.UserData;
+import com.example.carsharing.dto.CreateUserDto;
+import com.example.carsharing.userModule.User;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
+import static org.mockito.Mockito.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.mockito.Mockito;
 
+@ExtendWith(MockitoExtension.class)
 public class UserDataTest {
 
-    private static final String TEST_FILE_PATH = "testData.json";
+    @Mock
+    private FileReader mockFileReader;
+
+    @Mock
+    private FileWriter mockFileWriter;
+
+    @Mock
+    private JSONParser mockParser;
+
+    @InjectMocks
     private UserData userData;
+
+    private CreateUserDto newUserDto;
+    private JSONObject userObject;
+    private JSONArray usersArray;
 
     @BeforeEach
     public void setUp() throws Exception {
-        // Створення прикладу JSON масиву
-        JSONArray usersArray = new JSONArray();
-        JSONObject user1 = new JSONObject();
-        user1.put("id", 1L);
-        user1.put("email", "test1@example.com");
-        usersArray.add(user1);
+        // Initialize user DTO
+        newUserDto = new CreateUserDto("test@example.com", "TestUser", "password123");
 
-        // Запис даних до тестового файлу
-        try (FileWriter file = new FileWriter(TEST_FILE_PATH)) {
-            file.write(usersArray.toJSONString());
-            file.flush();
-        }
+        // Initialize JSON objects to simulate users
+        userObject = new JSONObject();
+        userObject.put("id", 1);
+        userObject.put("email", "test@example.com");
+        userObject.put("nickname", "TestUser");
+        userObject.put("password", "hashedpassword");
 
-        userData = new UserData(TEST_FILE_PATH);
+        usersArray = new JSONArray();
+        usersArray.add(userObject);
+
+        // Setup Mocks
+        when(mockParser.parse(any(FileReader.class))).thenReturn(usersArray);
+        doNothing().when(mockFileWriter).write(anyString());
+        doNothing().when(mockFileWriter).flush();
     }
 
-    @Test
-    public void testGetById() throws Exception {
-        JSONObject user = userData.get(1L);
-        assertNotNull(user);
-        assertEquals("test1@example.com", user.get("email"));
-    }
 
-    @Test
-    public void testGetByEmail() throws Exception {
-        JSONObject user = userData.get("test1@example.com");
-        assertNotNull(user);
-        assertEquals(1L, user.get("id"));
-    }
-
-    @Test
-    public void testGetAll() throws Exception {
-        JSONArray users = userData.get();
-        assertNotNull(users);
-        assertEquals(1, users.size());
-    }
-
-    */
-/*@Test
-    public void testCreate() throws Exception {
-        User newUser = new User();
-
-        dataWriter.create(newUser);
-        JSONObject retrievedUser = dataWriter.get("test2@example.com");
-        assertNotNull(retrievedUser);
-        assertEquals(2L, retrievedUser.get("id"));
-    }*//*
-
-
-    @Test
-    public void testUpdate() throws Exception {
-        JSONObject updatedUser = new JSONObject();
-        updatedUser.put("id", 1L);
-        updatedUser.put("email", "updated@example.com");
-
-        userData.update(1L, updatedUser);
-        JSONObject retrievedUser = userData.get("updated@example.com");
-        assertNotNull(retrievedUser);
-        assertEquals(1L, retrievedUser.get("id"));
-    }
-
-    @Test
-    public void testDelete() throws Exception {
-        userData.delete(1L);
-        JSONObject user = userData.get(1L);
-        assertNull(user);
-    }
-
-    // Після виконання тестів видалити тестовий файл
     @AfterEach
-    public void tearDown() throws Exception {
-        File file = new File(TEST_FILE_PATH);
-        if (file.exists()) {
-            file.delete();
-        }
+    public void tearDown() {
+        Mockito.reset(mockFileReader, mockFileWriter, mockParser);
     }
-    */
-/*@Test
-    public void testCreateWithExistingEmail() {
-        JSONObject newUser = new JSONObject();
-        newUser.put("id", 3L);
-        newUser.put("email", "test1@example.com");
 
-        Exception exception = assertThrows(Exception.class, () -> {
-            dataWriter.create(newUser);
-        });
+    @Test
+    public void testGetUsers() throws Exception {
+        JSONArray result = userData.get();
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        // Add more assertions as necessary
+    }
 
-        String expectedMessage = "Користувач з таким email вже існує";
-        String actualMessage = exception.getMessage();
-        assertTrue(actualMessage.contains(expectedMessage));
-    }*//*
+    @Test
+    public void testGetUserById() throws Exception {
+        JSONObject result = userData.get(1);
+        assertNotNull(result);
+        assertEquals("test@example.com", result.get("email"));
+        // Add more assertions as necessary
+    }
+
+    @Test
+    public void testGetUserByEmail() throws Exception {
+        JSONObject result = userData.get("test@example.com");
+        assertNotNull(result);
+        assertEquals(1, result.get("id"));
+        // Add more assertions as necessary
+    }
+
+    @Test
+    public void testCreateUser() throws Exception {
+        User result = userData.create(newUserDto);
+        assertNotNull(result);
+        assertEquals("test@example.com", result.getEmail());
+        // Add more assertions as necessary
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+        JSONObject updatedUser = new JSONObject();
+        updatedUser.put("nickname", "UpdatedTestUser");
+
+        userData.update(1, updatedUser);
+
+        // Verify save method was called
+        verify(mockFileWriter, times(1)).write(anyString());
+        // Additional verification and assertions as needed
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception {
+        userData.delete(1);
+
+        // Verify save method was called
+        verify(mockFileWriter, times(1)).write(anyString());
+        // Additional verification and assertions as needed
+    }
+
+
 
 }
-*/
